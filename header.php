71084fe7eaddfcabf101675d42c1af11693c9b19
nirmal2013 <nirmal2013@gmail.com>
2017-08-05 18:47:27 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<! DOCTYPE html>
+
+<head>
+
+  <title>
+
+    Marquee Visuals
+  </title>
+
+  <link rel = "stylesheet"
+     type = "text/css"
+     href = "css/style.css" />
+
+</head>
+
+<body>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+
+
+
+<img src="images/marqueeunderconstruction.png" alt="Marquee coming soon">
+
+
+<?php include 'footer.php'; ?>
